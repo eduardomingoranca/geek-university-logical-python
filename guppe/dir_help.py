9898824207c139c621307a56614da006a30228e0
eduardomingoranca <eduardomingoranca@gmail.com>
2023-10-12 14:15:06 -0300

    dir e help

---
+++ guppe/dir_help.py
@@ -0,0 +1,12 @@
+"""
+Utilitarios Python para auxiliar na programacao.
+
+Dir -> Apresenta todos os atributos e funcoes/metodos disponiveis
+para determinado tipo de dado ou variavel.
+
+Help -> Apresenta a documentacao/como utilizar os atributos/propriedades e funcoes/metodos disponiveis
+para determinado tipo de dado ou variavel.
+
+help(tipo de dado/variavel.propriedade)
+"""
+print(dir('geek'))
