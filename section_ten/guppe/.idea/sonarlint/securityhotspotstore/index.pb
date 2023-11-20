3c6bb51e98548d7e1df93f57185f372445c3aeb4
eduardomingoranca <eduardomingoranca@gmail.com>
2023-11-20 15:38:06 -0300

    zip

--- section_ten/guppe/.idea/sonarlint/securityhotspotstore/index.pb
+++ section_ten/guppe/.idea/sonarlint/securityhotspotstore/index.pb
@@ -22,3 +22,5 @@ min_max.py,9\c\9c4e13f4370474c1641ecc7352ca1c508980012a
 reversed.py,4\8\484974d4a8cc69b3af44cc670fb45055a25c90f7
 D
 len_abs_sum_round.py,6\4\644cfc0240d10a0b559a7fcca1039ab0ed9f690b
+6
+zip.py,9\a\9a6df8abb649fef485d483672ea2898ebe2e5399
---
+++ section_ten/guppe/zip.py
@@ -0,0 +1,70 @@
+"""
+Zip
+
+zip() -> Cria um iteravel (Zip Object) que agrega elemento de cada um dos iteraveis passados como entrada em pares.
+
+# Exmplos
+
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6, 12]
+
+zip1 = zip(lista1, lista2, 'abc')
+
+print(zip1)
+print(type(zip1))
+
+# Sempre podemos gerar uma Lista, Tupla, ou Dicionario
+
+print(list(zip1))
+
+
+zip1 = zip(lista1, lista2, 'abc')
+print(tuple(zip1))
+
+zip1 = zip(lista1, lista2, 'abc')
+print(set(zip1))
+
+zip1 = zip(lista1, lista2)
+print(dict(zip1))
+
+
+# OBS: O zip() utiliza como parametro o menor tamanho em iteravel. Isso significa que se estiver
+# trabalhando com iteraveis de tamanhos diferentes, ira parar quando os elementos do menor
+# iteravel acarbar.
+
+lista3 = [7, 8, 9, 10, 11]
+
+zip1 = zip(lista3, lista2, lista1)
+print(list(zip1))
+
+# Podemos utilizar diferentes iteraveis com zip
+
+tupla = 1, 2, 3, 4, 5
+lista = [6, 7, 8, 9, 10]
+dicionario = {'a': 11, 'b': 12, 'c': 13, 'd': 14, 'e': 15}
+
+zt = zip(tupla, lista, dicionario.values())
+print(list(zt))
+
+# Lista de tuplas
+
+dados = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
+
+print(list(zip(*dados)))
+"""
+
+# Exemplos mais complexos
+
+prova1 = [80, 91, 78]
+prova2 = [98, 89, 53]
+alunos = ['mary', 'peter', 'carla']
+
+final = {dado[0]: max(dado[1], dado[2]) for dado in zip(alunos, prova1, prova2)}
+
+print(final)
+
+# Podemos utilizar o map()
+
+final = zip(alunos, map(lambda nota: max(nota), zip(prova1, prova2)))
+
+print(dict(final))
