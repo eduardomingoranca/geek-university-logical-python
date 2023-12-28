f53d30fb9ffc1409fa4aebd7495035e2069589c5
eduardomingoranca <eduardomingoranca@gmail.com>
2023-12-27 21:20:45 -0300

    escrevendo em arquivos CSV

--- section_eighteen/guppe/.idea/sonarlint/issuestore/index.pb
+++ section_eighteen/guppe/.idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,7 @@
 lendo_csv.py,6\8\6808572acc20a7c49ad69e30e570e5745a5aa7fd
 C
 files/lutadores.csv,2\c\2c357d72a78bfc9b14993b54d2a213cd24ce9025
+A
+escrevendo_csv.py,6\6\66a62cdb6795d42bfd48184d4a249aa7b1c1cdc5
+A
+files/filmes3.csv,4\3\4311675d8acc064c8f8a57b8c1c59d92bad64afd
---
+++ section_eighteen/guppe/escrevendo_csv.py
@@ -0,0 +1,45 @@
+"""
+Escrevendo em arquivos CSV
+
+reader() (leitor), writer() (escritor)
+
+writerow() -> Escreve uma linha
+
+# writer() -> Gera um objeto para que possamos escrever em um arquivo CSV. Utilizamos o metodo
+# writerow() para escrever cada linha. Este metodo recebe uma lista.
+
+from csv import writer
+
+
+with open('filmes.csv', 'w') as arquivo:
+    escritor_csv = writer(arquivo)
+    filme = None
+    escritor_csv.writerow(['Titulo', 'Genero', 'Duracao'])
+    while filme != 'sair':
+        filme = input('Informe o nome do filme: ')
+        if filme != 'sair':
+            genero = input('Informe o genero: ')
+            duracao = input('Informe a duracao (em minutos): ')
+            escritor_csv.writerow([filme, genero, duracao])
+
+"""
+
+
+# DictWriter
+
+# OBS: As chaves do dicionario devem ser as mesmas utilizadas como cabecalho.
+
+from csv import DictWriter
+
+
+with open('files/filmes3.csv', 'w') as arquivo:
+    cabecalho = ['Titulo', 'Genero', 'Duracao']
+    escritor_csv = DictWriter(arquivo, fieldnames=cabecalho)
+    escritor_csv.writeheader()
+    filme = None
+    while filme != 'sair':
+        filme = input('Informe o nome do filme: ')
+        if filme != 'sair':
+            genero = input('Informe o genero: ')
+            duracao = input('Informe a duracao (em minutos): ')
+            escritor_csv.writerow({"Titulo": filme, "Genero": genero, "Duracao": duracao})
