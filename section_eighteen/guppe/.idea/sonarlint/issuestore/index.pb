260f46551369c45f8787c840330e43306e615ecc
eduardomingoranca <eduardomingoranca@gmail.com>
2023-12-27 21:25:27 -0300

    conhecendo o pickle

--- section_eighteen/guppe/.idea/sonarlint/issuestore/index.pb
+++ section_eighteen/guppe/.idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,7 @@ A
 escrevendo_csv.py,6\6\66a62cdb6795d42bfd48184d4a249aa7b1c1cdc5
 A
 files/filmes3.csv,4\3\4311675d8acc064c8f8a57b8c1c59d92bad64afd
+D
+conhecendo_pickle.py,8\6\86e259723f16d5488e81aacac099461e73202728
+D
+files/animais.pickle,8\7\87315c051901dc18066780082a7f004e4100fef4
---
+++ section_eighteen/guppe/conhecendo_pickle.py
@@ -0,0 +1,71 @@
+"""
+Conhecendo o Pickle
+
+A funcao do Pickleerealizar o seguinte processo:
+
+Objeto Python -> Binarizacao
+
+Binarizacao -> Objeto Python
+
+Este processoechamado de serializacao/deserializacao
+
+#OBS: O modulo Pickle naoeseguro contra dados maliciosos eh deste forma
+naoerecomendado trabalhar com arquivos pickle vindos de outras pessoas
+que voce nao conheca ou de fontes desconhecidas.
+
+# Fazendo a escrita em arquivos pickle
+
+
+felix = Gato('Felix')
+pluto = Cachorro('Pluto')
+
+with open('animais.pickle', 'wb') as arquivo:
+    pickle.dump((felix, pluto), arquivo)
+"""
+
+import pickle
+
+
+class Animal:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    def comer(self):
+        print(f'{self.__nome} esta comendo...')
+
+
+class Gato(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def mia(self):
+        print(f'{self.nome} esta miando...')
+
+
+class Cachorro(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def late(self):
+        print(f'{self.nome} esta latindo...')
+
+
+# Fazer a leitura de dados em arquivos pickle
+
+
+with open('files/animais.pickle', 'rb') as arquivo:
+    gato, cachorro = pickle.load(arquivo)
+    print(f'O gato chama-se {gato.nome}')
+    gato.mia()
+    print(f'O tipo do gatoe{type(gato)}')
+
+    print(f'O cachorro chama-se {cachorro.nome}')
+    cachorro.late()
+    print(f'O tipo do cachorroe{type(cachorro)}')
