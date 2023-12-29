7b645bb93b2dd3801c406f364113ed9e8e867f89
eduardomingoranca <eduardomingoranca@gmail.com>
2023-12-28 21:24:11 -0300

    type hinting

---
+++ section_twenty_two/guppe/aula04.py
@@ -0,0 +1,17 @@
+#def cumprimentar(nome: str) -> str:
+#    return f'Ola, {nome}'
+#
+#
+#print(cumprimentar('Geek'))
+
+def cabecalho(texto: str, alinhamento: bool = True) -> str:
+    if alinhamento:
+        return f"{texto.title()}\n{ '-' * len(texto)}"
+    else:
+        return f" {texto.title()} ".center(50, '#')
+
+print(cabecalho('geek university'))
+
+print(cabecalho('geek university', alinhamento=False))
+
+print(cabecalho('geek university', alinhamento='geek'))
