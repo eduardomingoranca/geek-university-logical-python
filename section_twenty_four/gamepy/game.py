afe6aa26a0b81982ed7103946e46f6ee5a7849d1
eduardomingoranca <eduardomingoranca@gmail.com>
2023-12-28 22:51:41 -0300

    implementacao

--- section_twenty_four/gamepy/game.py
+++ section_twenty_four/gamepy/game.py
@@ -7,8 +7,26 @@ def main() -> None:
 
 
 def jogar(pontos: int) -> None:
-    # TODO document why this method is empty
-    pass
+    dificuldade: int = int(input('Informe o nivel de dificulade desejado [1, 2, 3 ou 4]: '))
+
+    calc: Calcular = Calcular(dificuldade)
+
+    print('Informe o resultado para a seguinte operacacao: ')
+    calc.mostrar_operacao()
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Voce tem {pontos} pontos(s).')
+
+    continuar: int = int(input('Deseja continuar no jogo? [1 - sim, 0 - nao] '))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f'Voce finalizou com {pontos} ponto(s).')
+        print('Ate a proxima!')
 
 
 if __name__ == '__main__':
