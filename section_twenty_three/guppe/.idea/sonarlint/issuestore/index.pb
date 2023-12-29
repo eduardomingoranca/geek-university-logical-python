a8f2cc91c263c3c2d8f68bb70a00288d4121c256
eduardomingoranca <eduardomingoranca@gmail.com>
2023-12-28 22:01:19 -0300

    debugger mais simples com f-strings

--- section_twenty_three/guppe/.idea/sonarlint/issuestore/index.pb
+++ section_twenty_three/guppe/.idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,5 @@ Q
 !argumentos_somente_posicionais.py,2\f\2fd38aa2f6e18ad7e31d6756f97965dccb68b8ce
 O
 tipos_de_dados_mais_precisos.py,a\1\a19a64b740ab290d084abe75280970862e9c576d
+D
+debugger_fstrings.py,c\4\c4e1bf84854ba253798a519f2ae38a47179f62b8
---
+++ section_twenty_three/guppe/debugger_fstrings.py
@@ -0,0 +1,18 @@
+# def multiplicar(num1: float, num2: float) -> float:
+#     return num1 * num2
+#
+# resultado: float = multiplicar(4.2345, 6.7890)
+#
+# print(f'O resultado eh {resultado}')
+#
+# print(f'O resultado eh {multiplicar(9, 4):.2f}')
+#
+# print(f'{(media := 8 / 2)} eh a metade de {media * 2}')
+
+geek: str = 'Geek University'
+
+print(f"geek='{geek}'")
+
+print(f'{geek=}')
+
+print(f'{geek.upper()[::-1] = }')
